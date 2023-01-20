3baa3c819a7b2e4b3a315d7793d2e7cc6a96a79f
White Egg <2733259150@qq.com>
2023-01-20 11:01:38 +0800

    添加商家详情样式

--- src/views/detail/detailitem/detailGoodsInfo.vue
+++ src/views/detail/detailitem/detailGoodsInfo.vue
@@ -1,7 +1,22 @@
 <template>
     <div class="goodsinfo">
-        <div class="goodsinfo_desc">{{ detailInfo.desc }}</div>
-        <div class="goodinfo_title">{{ title }}</div>
+        <div class="goodsinfo_desc">
+            <p class="goodsinfo_desc_left">
+                ■<span
+                    >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span
+                >
+            </p>
+            {{ detailInfo.desc }}
+            <p class="goodsinfo_desc_right">
+                <span class="goodsinfo_desc_right_nbsp"
+                    >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span
+                >
+                <span>■</span>
+            </p>
+        </div>
+        <div class="goodinfo_title">
+            <p>{{ title }}</p>
+        </div>
         <div class="goodinfo_image">
             <a :href="item" v-for="item in image" :key="item">
                 <img :src="item" :alt="item" />
@@ -50,11 +65,40 @@ export default {
 </script>
 
 <style  scoped>
-.goodinfo_image{
-    width: 100vw;
+.goodsinfo_desc {
+    color: #3e3c3ce6;
 }
-.goodsinfo_image a img {
-    height: 100%;
+.goodsinfo_desc_left span,
+.goodsinfo_desc_right .goodsinfo_desc_right_nbsp {
+    text-decoration: underline;
+}
+.goodsinfo_desc_left span {
+    position: relative;
+    top: -3px;
+    margin-left: -2px;
+}
+.goodsinfo_desc_right {
+    width: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+}
+
+.goodsinfo_desc_right_nbsp {
+    margin-right: -2px;
+    position: relative;
+    top: -3px;
+}
+.goodinfo_image a {
+    width: 100%;
+}
+.goodinfo_image {
+    width: 100vw;
+    display: flex;
+    flex-direction: column;
+}
+.goodinfo_image a img {
+    flex: 1;
     width: 100%;
 }
 </style>
