ba222040e9f12f0fb51b6ba374503189379179df
White Egg <2733259150@qq.com>
2023-02-23 16:23:23 +0800

    加了点购物车样式，不用复选框因为样式因为默认框框不好看

--- src/views/cart/cartitem/CartItemData.vue
+++ src/views/cart/cartitem/CartItemData.vue
@@ -1,11 +1,29 @@
 <template>
-    <div>
-        <div v-for="(item) in cartLists" :key="item.iid">
+    <div id="cart">
+        <div
+            v-for="(item, index) in cartLists"
+            :key="item.iid"
+            class="cartitem"
+            :class="{ first_cartitem: index == 0 }"
+        >
+            <div class="cartitem_show">
+                <span
+                    class="cartitem_show_checkbox"
+                    :class="{ cartitem_show_checkbox_on: true }"
+                ></span>
+                <img :src="item.image" :alt="item.title" />
+            </div>
+            <div class="cartitem_data">
+                <span class="cartitem_data_title"> {{ item.title }}</span>
+                <span class="cartitem_data_desc">{{ item.desc }}</span>
+                <div class="cartitem_data_price">
+                    <span>{{ "￥" + item.price }}</span
+                    ><span class="cartitem_data_price_price">{{
+                        item.quantity + "件"
+                    }}</span>
+                </div>
+            </div>
             <!-- {{ item }} -->
-            <p>{{item.title}}</p>
-            <p>{{item.desc}}</p>
-            <div><span>{{item.price}}</span><span>{{item}}</span></div>
-            <img :src="item.image" :alt="item.title" />
         </div>
     </div>
 </template>
@@ -33,7 +51,70 @@ export default {
 </script>
 
 <style  scoped>
-p{
-    margin: 0;
+.cartitem_show_checkbox_on {
+    background-color: #0015ff !important;
+    border: 0 !important;
+    overflow: hidden;
+}
+.cartitem_show_checkbox_on::after {
+    content: "√";
+    font-size: 16px;
+    font-weight: 900;
+    color: #fff;
+}
+.first_cartitem {
+    margin-top: 0 !important;
+}
+.cartitem {
+    display: flex;
+    width: 100%;
+    margin: 0.5rem 0;
+}
+
+.cartitem .cartitem_show {
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+}
+.cartitem .cartitem_show img {
+    width: 70%;
+    border-radius: 5px;
+}
+.cartitem .cartitem_show .cartitem_show_checkbox {
+    display: inline-block;
+    width: 0.8rem;
+    height: 0.8rem;
+    margin: 0 0.3rem;
+    border-radius: 35px;
+    border: 0.1px solid #000;
+    background: #f8f8f8;
+}
+.cartitem .cartitem_data {
+    flex: 0.7;
+    display: flex;
+    flex-direction: column;
+}
+.cartitem .cartitem_data .cartitem_data_title,
+.cartitem .cartitem_data .cartitem_data_desc {
+    font-size: 15px;
+    width: 70vw;
+    padding: 0.5rem 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.cartitem .cartitem_data .cartitem_data_desc {
+    font-size: 10px;
+    color: #7d7d7d;
+}
+.cartitem .cartitem_data .cartitem_data_price {
+    display: flex;
+    justify-content: space-between;
+    padding: 1rem 1rem 1rem 0;
+    color: red;
+}
+.cartitem .cartitem_data .cartitem_data_price .cartitem_data_price_price {
+    color: #000;
+    font-weight: 900;
 }
 </style>
