b88001b17259c08eb34d5d78ec982186a0f793e0
White Egg <2733259150@qq.com>
2023-05-18 18:40:15 +0800

    添加了一些注释

--- src/network/user.js
+++ src/network/user.js
@@ -1,4 +1,4 @@
-
+// 这里需要自建个本地服务器解决
 import { requestSelfAPI, requestSelfPostAPI } from "./request";
 
 export function userGet() {
@@ -7,6 +7,7 @@ export function userGet() {
     })
 }
 export function userpost() {
+    // 首先后端有这个处理才可以使用，提交给body处理
     return requestSelfPostAPI(
         "postapi/user",
         JSON.stringify({ 'body': 'datadadadad', "ddd": "ddd" }),
